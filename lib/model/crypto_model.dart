66e8d1b30a21d8ae5e600e6e04764790288ef23c
burshtynv <vanyaburshtyn@gmail.com>
2021-02-02 11:50:57 +0300

    new

---
+++ lib/model/crypto_model.dart
@@ -0,0 +1,15 @@
+class CryptoModel {
+  final String name;
+  final String symbol;
+  final int rank;
+  final double price;
+  final double explorer;
+
+  const CryptoModel({
+    this.name,
+    this.symbol,
+    this.rank,
+    this.price,
+    this.explorer,
+  });
+}
